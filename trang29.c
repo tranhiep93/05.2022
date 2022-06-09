90ff3034262f77aa66fae42d6b79e045d82ef0b9
tranhiep93 <106593009+tranhiep93@users.noreply.github.com>
2022-06-09 21:16:00 +0700

    task9.t.x.hiep

---
+++ trang29.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a=10;
+	float b=24.67892345;
+	char ch='A';
+	
+	printf("Integer data = %d\n", a);
+	printf("Float data = %f\n",b);
+	printf("Character = %c\n",ch);
+	printf("this prints the string\n");
+	printf("%s","this also prints a string\n");
+
+	return 0;
+}
